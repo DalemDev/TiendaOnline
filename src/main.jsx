5d07b6b1025043606880f24dc1836c4f82f47372
dalemdev <developerj659@gmail.com>
2024-12-02 19:17:54 +0000

    configurando rutas y pagina 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,25 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import './assets/styles/index.css'
 
+const rutas = [
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '*',
+    element: <h1>404</h1>
+  }
+];
+
 createRoot(document.getElementById('root')).render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      {rutas.map((ruta, index) => (
+        <Route key={index} {...ruta} />
+      ))}
+    </Routes>
+  </BrowserRouter>
 )
---
+++ src/pages/404/index.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+export default function index() {
+  return (
+    <div class="not-found">
+      <h1>404</h1>
+      <div class="animation">😔</div>
+      <p>Oops! The page you're looking for doesn't exist.</p>
+      <a href="/">Go Back Home</a>
+    </div>
+  )
+}
