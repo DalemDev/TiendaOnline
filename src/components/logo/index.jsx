74c4e704361b23d3d115f218a5306cbc919811cb
dalemdev <jdeveloper659@gmail.com>
2024-12-03 02:30:45 +0000

    agregando componente logo

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+export default function index() {
+  return (
+    <div className="logo">
+      <h1 className="logo-title">Shopalooza</h1>
+      <p className="logo-slogan">donde las compras son una fiesta</p>
+    </div>
+  );
+}
